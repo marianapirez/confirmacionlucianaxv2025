844f78d30d0121e448696fca41162340ef403215
marianapirez <marianapirezds@gmail.com>
2025-03-29 19:17:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,8 @@ const invitados = {
    "jazminrivero": { nombre: "Jazmin Rivero", telefono: "098062436", cupos: 1 },
    "aaronmendiondo": { nombre: "Aarón Mendiondo", telefono: "093883670", cupos: 1 },
    "helenmachado": { nombre: "Helen Machado", telefono: "096937218", cupos: 1 },
-
+   "luanasuarez": { nombre: "Luana Suárez", telefono: "992947027", cupos: 1 },
+  
 // Lista de invitados Familia (103) + 5 de nosotros (no estamos en lista)
     "makinsondossantos": { nombre: "Makinson dos Santos", telefono: "095608348", cupos: 1 },
     "sandradossantos": { nombre: "Sandra dos Santos", telefono: "096112224", cupos: 2 },
